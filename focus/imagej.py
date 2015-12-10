6fde8e29ac2337a4471941637c3664e441d0b309
Kaylea Nelson <kaylea.nelson@yale.edu>
2015-12-10 11:11:29 -0500

    first pass at ImageJ support

---
+++ focus/imagej.py
@@ -0,0 +1,134 @@
+"""
+imagej.py
+
+Adapted from batchIJSF.py
+
+Script for batch focusing of segment output using StackFocuser
+plugin for ImageJ. Generates grayscale focused image and heightmap
+for all objects.
+
+Requires an installation of FIJI (http://fiji.sc/Fiji) that is
+accessible from the path via the following commands:
+
+    Mac:    'ImageJ-macosx'
+    Linux:  'ImageJ-linux32' OR 'ImageJ-linux64'
+    
+This program will overwrite existing files from previous runs, so
+please be careful!
+
+(NOTE: AT THE MOMENT, THIS ONLY WORKS FOR LINUX/TIDE!)
+"""
+
+import os
+import glob
+
+
+def run(directories):
+    """
+    focusedPath: full path to the folder containing the output from
+    the AutoMorph 'focus' software.
+
+    sfPath: full path to the 'stackfocused' folder, into which files
+    will be outputted
+
+    kernelSize: an odd integer specifying kernel size for heightmap
+    generation in StackFocuser.
+
+    fijiArchitecture: an integer in [32,64] that specifies the
+    architecture of the installed version of FIJI (default value
+    is 32; may be left alone if using MacOSX).
+    """
+
+    # os.chdir(directories['stripped'])
+
+    system_platform = platform.system()
+
+    if system_platform == 'Linux':
+        if software['fiji_architecture'] == 32:
+            architecture = 'linux32'
+        elif software['fiji_architecture'] == 64:
+            architecture = 'linux64'
+        else:
+            raise ValueError('FIJI architecture inappropriately specified (must be 32 or 64)')
+    elif system_platform == 'Darwin':
+        architecture = 'macosx'
+    else:
+        raise ValueError('Inappropriate system (must be Linux or Darwin)')
+
+    stripped_objects = [os.path.realpath(x) for x in glob.glob(os.path.join(directories["stripped"],
+                                                                            '*_obj*'))]
+
+    print 'Begin FIJI processing...\n'
+
+    command = 'ImageJ-' + sysarchSpecifier + ' --headless --memory=1000m -macro '
+
+    for stripped_object in stripped_objects:
+        start = time.time()
+        print 'Object: ' + stripped_object
+
+        batch_file = write_batchfile(stripped_object, directories, software)
+
+        subprocess.call(command+batch_file, shell=True)
+
+        end = time.time()
+        time_elapsed = end - start
+        print 'Time Elapsed: ' + '%6.3f' % time_elapsed + ' seconds\n'
+
+    # check 
+    first_object = os.path.realpath(os.path.join(directories['stripped'],
+                                                 os.path.basename(directories["objects"][0]),
+                                                 'ij_focused.tif'))
+    last_object = os.path.realpath(os.path.join(directories['stripped'],
+                                                os.path.basename(directories["objects"][-1]),
+                                                'ij_focused.tif'))
+
+    if not os.path.exists(first_object):
+        sys.exit("Error: ImageJ didn't create files. Perhaps something went wrong? Exiting...")
+    elif not os.path.exists(last_object):
+        sys.exit("Error: ImageJ didn't finish creating files. Perhaps something went wrong? Exiting...")
+    else:
+        print 'ImageJ finished!'
+
+
+def write_batchfile(stripped_object, directories, software):
+    """
+    imageStackDir: full path to folder containing stack of images for
+    an object
+
+    outputDir: full path to output directory for macro files
+
+    objName: string dictating object name (used as base for file name
+    for macro file)
+
+    kernelSize: an odd integer specifying kernel size for heightmap
+    generation in StackFocuser.
+    """
+    
+    obj_dir, obj_name = os.path.basename(stripped_object)
+    print obj_name
+
+    # Write individual macro file for object
+    macro_file_path = os.path.join(stripped_object,'macro.imj')
+    macro_file = open(macro_file_path,'w')
+    macro_text = """setBatchMode(true);
+run("Image Sequence...", "open={0} file=[.jpg] convert sort");
+run("Stack Focuser ", "enter={1} generate");
+selectWindow("Focused_{2}");
+saveAs("Tiff","{3}");
+selectWindow("HeightMap_{2}");
+saveAs("Tiff","{4}");
+selectWindow("{5}");
+close("*");
+"""
+
+    macro_text = macro_text.format(stripped_object,
+                                   software['kernel_size'],
+                                   obj_name,
+                                   os.path.join(stripped_object,'ij_focused.tif'),
+                                   os.path.join(stripped_object,'ij_heightmap.tif'),
+                                   objName)
+
+    macro_file.write(macro_text)
+    macro_fle.close()
+
+    return macro_file_path    
--- focus/zerene.py
+++ focus/zerene.py
@@ -3,6 +3,38 @@ import os
 import glob
 import pwd
 
+
+def run(directories, software):
+
+    # Initialize the batch XML file
+    write_batchfile(directories)
+
+    # Run the XML file
+    zerene_command = construct_command(software)
+    if software['verbose']:
+        zerene_command += ' %s 2>&1 | tee %s' % (os.path.join(directories['input'], 'zsbatch.xml'),
+                                                 os.path.join(directories['input'], 'zerene.log'))
+    else:
+        zerene_command += ' %s > %s' % (os.path.join(directories['input'], 'zsbatch.xml'),
+                                        os.path.join(directories['input'], 'zerene.log'))
+    print 'Running Zerene Stacker: ', zerene_command
+
+    subprocess.call(zerene_command, shell=True)
+
+    first_object = os.path.realpath(os.path.join(directories['stripped'],
+                                                 os.path.basename(directories["objects"][0]),
+                                                 'ZS.tif'))
+    last_object = os.path.realpath(os.path.join(directories['stripped'],
+                                                os.path.basename(directories["objects"][-1]),
+                                                'ZS.tif'))
+
+    if not os.path.exists(first_object):
+        sys.exit("Error: Zerene didn't create files. Perhaps something went wrong? Exiting...")
+    elif not os.path.exists(last_object):
+        sys.exit("Error: Zerene didn't finish creating files. Perhaps something went wrong? Exiting...")
+    else:
+        print 'Zerene Stacker finished!'
+
 def construct_command(software):
 
     zerene_dir = software['zerene_dir']
@@ -44,7 +76,11 @@ def construct_command(software):
     return zerene_command
 
 
-def write_batchfile(directories):
+
+
+def write_batchfile(directories, batch_type="default"):
+
+    batchxml_object = set_batchxml(batch_type)
 
     batchxml_header = """<?xml version="1.0" encoding="UTF-8"?>
     <ZereneStackerBatchScript>
@@ -53,8 +89,32 @@ def write_batchfile(directories):
                 <Batches length="{0}">
                       """.format(len(directories['objects']))
 
-    batchxml_object = """
-    <Batch>
+    batchxml_footer = """
+        </Batches>
+      </BatchQueue>
+    </ZereneStackerBatchScript>
+                      """
+
+    # Step 4: Loop over all object directories and write the ZereneStacker batch XML file:
+    with open(os.path.join(directories["input"], 'zsbatch.xml'), 'w') as zsbatch:
+
+        zsbatch.write(batchxml_header)
+
+        stripped_objects = [os.path.realpath(x) for x in glob.glob(os.path.join(directories["stripped"], '*_obj*'))]
+
+        for stripped_object in stripped_objects:
+            print 'Adding object to batch file: ', stripped_object
+            zsbatch.write(batchxml_object.format(stripped_object, directories["input"]))
+
+        zsbatch.write(batchxml_footer)
+        print 'Batch XML file written.'
+
+
+def set_batchxml(batch_type):
+
+    if batch_type == "DMAP":
+        batchxml_object = """
+          <Batch>
             <Sources length="1">
               <Source value="{0}" />
             </Sources>
@@ -156,22 +216,108 @@ def write_batchfile(directories):
           </Batch>
     """
 
-    batchxml_footer = """
-        </Batches>
-      </BatchQueue>
-    </ZereneStackerBatchScript>
-                      """
+    else:
+        batchxml_object = """
+          <Batch>
+            <Sources length="1">
+              <Source value="{0}" />
+            </Sources>
+            <ProjectDispositionCode value="101" />
+            <Tasks length="1">
+              <Task>
+                <OutputImageDispositionCode value="2" />
+                <OutputImagesDesignatedFolder value="{1}/focused/" />
+                <Preferences>
+                  <AcquisitionSequencer.BacklashMillimeters value="0.22" />
+                  <AcquisitionSequencer.CommandLogging value="false" />
+                  <AcquisitionSequencer.DistancePerStepperRotation value="1.5875" />
+                  <AcquisitionSequencer.MaximumMmPerSecond value="2.0" />
+                  <AcquisitionSequencer.MicrostepsPerRotation value="3200" />
+                  <AcquisitionSequencer.MovementRampTime value="2.0" />
+                  <AcquisitionSequencer.NumberOfSteps value="5" />
+                  <AcquisitionSequencer.PrecisionThreshold value="0.05" />
+                  <AcquisitionSequencer.PrerunMillimeters value="0.0" />
+                  <AcquisitionSequencer.RPPIndicatorLeft value="-100.0" />
+                  <AcquisitionSequencer.RPPIndicatorRight value="+100.0" />
+                  <AcquisitionSequencer.SettlingTime value="3.0" />
+                  <AcquisitionSequencer.ShutterActivationsPerStep value="1" />
+                  <AcquisitionSequencer.ShutterAfterTime value="2.0" />
+                  <AcquisitionSequencer.ShutterBetweenTime value="1.0" />
+                  <AcquisitionSequencer.ShutterPulseTime value="0.3" />
+                  <AcquisitionSequencer.StepSize value="0.1" />
+                  <AcquisitionSequencer.StepSizeAdjustmentFactor value="1.0" />
+                  <AcquisitionSequencer.StepSizesFile value="" />
+                  <AlignmentControl.AddNewFilesAsAlreadyAligned value="false" />
+                  <AlignmentControl.AlignmentSettingsChanged value="false" />
+                  <AlignmentControl.AllowRotation value="false" />
+                  <AlignmentControl.AllowScale value="false" />
+                  <AlignmentControl.AllowShiftX value="false" />
+                  <AlignmentControl.AllowShiftY value="false" />
+                  <AlignmentControl.BrightnessSettingsChanged value="false" />
+                  <AlignmentControl.CorrectBrightness value="false" />
+                  <AlignmentControl.MaxRelDegRotation value="20" />
+                  <AlignmentControl.MaxRelPctScale value="20" />
+                  <AlignmentControl.MaxRelPctShiftX value="20" />
+                  <AlignmentControl.MaxRelPctShiftY value="20" />
+                  <AlignmentControl.Order.Automatic value="true" />
+                  <AlignmentControl.Order.NarrowFirst value="true" />
+                  <AllowReporting.UsageStatistics value="false" />
+                  <ColorManagement.DebugPrintProfile value="false" />
+                  <ColorManagement.InputOption value="Use_EXIF_and_DCF_rules" />
+                  <ColorManagement.InputOption.AssumedProfile value="sRGB IEC61966-2.1" />
+                  <ColorManagement.ManageZSDisplays value="false" />
+                  <ColorManagement.ManageZSDisplaysHasChanged value="false" />
+                  <ColorManagement.OutputOption value="CopyInput" />
+                  <DepthMapControl.AlgorithmIdentifier value="1" />
+                  <DepthMapControl.ContrastThresholdLevel value="0" />
+                  <DepthMapControl.ContrastThresholdPercentile value="25.0" />
+                  <DepthMapControl.EstimationRadius value="10" />
+                  <DepthMapControl.SaveDepthMapImage value="false" />
+                  <DepthMapControl.SaveDepthMapImageDirectory value="" />
+                  <DepthMapControl.SaveUsedPixelImages value="false" />
+                  <DepthMapControl.SmoothingRadius value="5" />
+                  <DepthMapControl.UseFixedContrastThresholdLevel value="false" />
+                  <DepthMapControl.UseFixedContrastThresholdPercentile value="true" />
+                  <DepthMapControl.UsedPixelFractionThreshold value="0.5" />
+                  <FileIO.UseExternalTIFFReader value="false" />
+                  <Interpolator.RenderingSelection value="Interpolator.Spline4x4" />
+                  <Interpolator.ShowAdvanced value="false" />
+                  <LightroomPlugin.CurrentInstallationFolder value="" />
+                  <LightroomPlugin.DefaultColorSpace value="AdobeRGB" />
+                  <OutputImageNaming.Template value="ZS" />
+                  <Precrop.LimitsString value="" />
+                  <Precrop.Selected value="false" />
+                  <Prerotation.Degrees value="0" />
+                  <Prerotation.Selected value="false" />
+                  <Presize.UserSetting.Scale value="1.0" />
+                  <Presize.UserSetting.Selected value="false" />
+                  <Presize.Working.Scale value="1.0" />
+                  <PyramidControl.GritSuppressionMethod value="1" />
+                  <PyramidControl.RetainUDRImage value="false" />
+                  <RetouchingBrush.Hardness value="0.5" />
+                  <RetouchingBrush.ShowBrushes value="false" />
+                  <RetouchingBrush.Type value="Details" />
+                  <RetouchingBrush.Width value="10" />
+                  <SaveImage.BitsPerColor value="8" />
+                  <SaveImage.CompressionQuality value="1.00" />
+                  <SaveImage.FileType value="tif" />
+                  <SaveImage.RescaleImageToAvoidOverflow value="false" />
+                  <SkewSequence.FirstImage.MaximumShiftXPct value="-3.0" />
+                  <SkewSequence.FirstImage.MaximumShiftYPct value="0.0" />
+                  <SkewSequence.LastImage.MaximumShiftXPct value="3.0" />
+                  <SkewSequence.LastImage.MaximumShiftYPct value="0.0" />
+                  <SkewSequence.NumberOfOutputImages value="3" />
+                  <SkewSequence.Selected value="false" />
+                  <StackingControl.FrameSkipFactor value="1" />
+                  <StackingControl.FrameSkipSelected value="false" />
+                  <StereoOrdering.LeftRightIndexSeparation value="1" />
+                  <WatchDirectoryOptions.AcceptViaDelay value="false" />
+                  <WatchDirectoryOptions.AcceptViaDelaySeconds value="2.0" />
+                </Preferences>
+                <TaskIndicatorCode value="2" />
+              </Task>
+            </Tasks>
+          </Batch>
+    """
 
-    # Step 4: Loop over all object directories and write the ZereneStacker batch XML file:
-    with open(os.path.join(directories["input"], 'zsbatch.xml'), 'w') as zsbatch:
-
-        zsbatch.write(batchxml_header)
-
-        stripped_objects = [os.path.realpath(x) for x in glob.glob(os.path.join(directories["stripped"], '*_obj*'))]
-
-        for stripped_object in stripped_objects:
-            print 'Adding object to batch file: ', stripped_object
-            zsbatch.write(batchxml_object.format(stripped_object, directories["input"]))
-
-        zsbatch.write(batchxml_footer)
-        print 'Batch XML file written.'
+    return batchxml_object
