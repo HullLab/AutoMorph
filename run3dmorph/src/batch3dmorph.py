e7c28e9b2aaaff2a5d41e192f81baa8d4fee7875
Kaylea Nelson <kaylea.nelson@yale.edu>
2017-03-27 13:30:37 -0400

    more fixes to run3dmorph directory creation

--- run3dmorph/src/batch3dmorph.py
+++ run3dmorph/src/batch3dmorph.py
@@ -109,9 +109,9 @@ if settings['savePDF'] == 'true' or settings['savePDF'] == '[]':
 	idtfPath = os.path.join(pdf3dPath,'idtf_files')
 	u3dPath = os.path.join(pdf3dPath,'u3d_files')
 	if not os.path.exists(u3dPath):
-		os.mkdirs(u3dPath)
+		os.makedirs(u3dPath)
 	if not os.path.exists(idtfPath):
-		os.mkdirs(idtfPath)
+		os.makedirs(idtfPath)
 	# Loop through IDTF files and convert to U3D
 	print '\tBuilding U3D files...'
 	# Check system architecture for writing IDTF command
