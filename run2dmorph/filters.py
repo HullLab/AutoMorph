9f629cdd0507a16ef5ecb2e54736c8ec6f91c51a
Allison Hsiang <allison.hsiang@gmail.com>
2017-06-09 23:16:59 +0200

    Removed cv dependency and fixed clearBorder floodFill calls

--- run2dmorph/filters.py
+++ run2dmorph/filters.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-import cv
 import cv2
 import numpy as np
 
@@ -80,18 +79,18 @@ def clearBorder(image,size):
     Deletes binary objects that touch the border of the image.
     '''
     h,w = size[:2]
-    image_noise = image.copy()
-    imgMat = cv.fromarray(image_noise)
+    image_noise = np.array(image.copy())
+    mask = np.zeros((h+2,w+2),np.uint8)
 
     # Loop through border pixels as seeds
     for x in range(w):
-        temp = cv.FloodFill(imgMat,(x,0),0)
-        temp = cv.FloodFill(imgMat,(x,h-1),0)
+        temp = cv2.floodFill(image_noise,mask,(x,0),0)
+        temp = cv2.floodFill(image_noise,mask,(x,h-1),0)
     for y in range(h):
-        temp = cv.FloodFill(imgMat,(0,y),0)
-        temp = cv.FloodFill(imgMat,(w-1,y),0)
+        temp = cv2.floodFill(image_noise,mask,(0,y),0)
+        temp = cv2.floodFill(image_noise,mask,(w-1,y),0)
 
-    image_border = np.asarray(imgMat)
+    image_border = np.asarray(image_noise)
     return image_border
 
 
