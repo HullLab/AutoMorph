072ad4732e2ff034840648a704b7c5278ea0c3fe
Allison Hsiang <allison.hsiang@gmail.com>
2018-04-13 12:02:42 +0200

    New coordinate ordering and downsampling algorithm

--- run2dmorph/extractcoordinates.py
+++ run2dmorph/extractcoordinates.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 
 import numpy as np
-from scipy import interpolate
+from scipy import interpolate, signal
 import pandas
+import math
+
+
+def getDistance(point1,point2):
+    '''
+    Calculates Euclidean distance between two points.
+    '''
+    return math.hypot(point2[0]-point1[0],point2[1]-point1[1])
 
 
 def extractCoordinates(edge,downsample,num_points,region_properties):
@@ -11,46 +19,35 @@ def extractCoordinates(edge,downsample,num_points,region_properties):
     to outline, downsampling if requested by user.
     '''
     all_coords = np.where(edge == 255)
-    x_coords = list(all_coords[0])
-    y_coords = list(all_coords[1])
+    x_coords = list(all_coords[1])
+    y_coords = list(-all_coords[0])
     num_coords = len(x_coords)
 
     # Translate coordinates so that centroid is at (0,0)
     centroid = region_properties.centroid
-    xc = [x - centroid[0] for x in x_coords]
-    yc = [y - centroid[1] for y in y_coords]
+    centroid_adjusted = (centroid[1],-centroid[0])
+    xc = [x - centroid_adjusted[0] for x in x_coords]
+    yc = [y - centroid_adjusted[1] for y in y_coords]
+    coords = np.array([(x,y) for x,y in zip(xc,yc)])
+
+    # Order points using nearest neighbor
+    ordered = [coords[0]]
+    processing = np.delete(coords,0,0)
+    
+    for i in range(len(coords)):
+        distances = [getDistance(ordered[i],processing[j]) for j in range(len(processing))] # Distances between i-th point and i+1-th point
+        min_index = np.argmin(distances)
+        ordered.append(processing[min_index])
+        processing = np.delete(processing,min_index,0)
+
+        if len(processing) == 1:
+            break
+
+    ordered = np.vstack(ordered) # Put points into correct data structure
 
     if downsample:
-        # ORDERING POINTS
-        # Convert cartesian coordinates to polar coordinate system
-        rho = [np.sqrt(x ** 2 + y ** 2) for x,y in zip(xc,yc)]
-        phi = [np.arctan2(y,x) for x,y in zip(xc,yc)]
-
-        # Sort by increasing order of phi
-        pr_sorted = sorted(zip(phi,rho))
-        phis = [p[0] for p in pr_sorted]
-        rhos = [r[1] for r in pr_sorted]
-
-        # Convert back to cartesian coordinates (now ordered)
-        xs = [r * np.cos(p) for p,r in zip(phis,rhos)]
-        ys = [r * np.sin(p) for p,r in zip(phis,rhos)]
-
-        # INTERPOLATION
-        # First decimate points naively by slicing
-        slice_factor = int(round(num_coords / num_points))
-        xd = xs[::slice_factor]
-        yd = ys[::slice_factor]
-
-        # Close circle by appending first coordinate to end of coordinate list
-        xdr = np.r_[xd,xd[0]]
-        ydr = np.r_[yd,yd[0]]
-
-        # Interpolate using closed, ordered, decimated coordinates
-        tck,u = interpolate.splprep([xdr,ydr],s=0,per=True)
-        xi,yi = interpolate.splev(np.linspace(0,1,num_points),tck)
-
-        return np.column_stack((yi,-xi))
+        resampled = signal.resample(ordered,num_points)
+        return resampled
 
     else:
-
-        return np.column_stack((np.array(yc),-np.array(xc)))
+        return ordered
